8bc0d74d82cb05d6f04bab643740a95bd79fcbd1
huangjian <huangjian921@outlook.com>
2025-08-03 17:47:45 +0800

    add mdk project for bootloader

--- board/art-pi/projects/bootloader/Core/Src/main.c
+++ board/art-pi/projects/bootloader/Core/Src/main.c
@@ -79,8 +79,18 @@ static void CPU_CACHE_Disable(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-int __io_putchar(int ch)
-{
+#if defined(__ARMCC_VERSION)
+#define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)
+#else
+/* With GCC/RAISONANCE, small printf (option LD Linker->Libraries->Small printf
+   set to 'Yes') calls __io_putchar() */
+#define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
+#endif /* __CC_ARM */
+
+PUTCHAR_PROTOTYPE {
+  /* Place your implementation of fputc here */
+  /* e.g. write a character to the USART1 and Loop until the end of transmission
+   */
   HAL_UART_Transmit(&huart4, (uint8_t *)&ch, 1, 0xFFFF);
   return ch;
 }
